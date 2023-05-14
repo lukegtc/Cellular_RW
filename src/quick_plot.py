354bad69c2a5f9840469f3d65addff63000eb69a
lukegtc <lukegtc@gmail.com>
2023-05-14 17:25:40 +0200

    refactored

---
+++ src/quick_plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import os
+cwd = os.getcwd()
+mpgnn = f'{cwd}/MPGNN/lightning_logs/version_0/metrics.csv'
+pe_mpgnn = f'{cwd}/PEMPGNN/lightning_logs/version_0/metrics.csv'
+def plot(filename, label):
+
+    df = pd.read_csv(filename)
+    val_loss = df['val_loss'].dropna()
+    plt.plot(val_loss, label=label)
+
+
+
+plot(mpgnn, 'MPGNN')
+plot(pe_mpgnn, 'PE-MPGNN')
+plt.xlabel('Epochs')
+plt.ylabel('Validation Loss')
+plt.title('Validation Losses')
+plt.grid()
+plt.legend()
+plt.show()
+plt.savefig('Validation_Loss_plot.png')
